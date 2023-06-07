190791e1d15f195da53af1a1e7b00373ef6d6319
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-06-07 10:26:04 -0500

    Validacion de email

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,10 +1,13 @@
 const { response, request } = require('express');
 const bcryptjs = require('bcryptjs');
 
-const Usuario = require('../models/usuario')
+const { validationResult } = require('express-validator');
+
+const Usuario = require('../models/usuario');
 
 
 const usuariosGet = (req = request, res = response) => {
+
 
     const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
 
@@ -20,11 +23,24 @@ const usuariosGet = (req = request, res = response) => {
 
 const usuariosPost = async (req, res = response) => {
 
+    const errors = validationResult (req);
+    if(!errors.isEmpty()){
+        return res.status(400).json(errors);
+    }
+
+    
+
     const {nombre,correo,password,rol} = req.body;
     const usuario = new Usuario ({nombre,correo,password,rol});
 
 
     //Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail){
+        return res.status(400).json({
+            msg: 'El correo ya se encuentra registrado'
+        });
+    }
 
     //Encriptar la contraseña
     const salt = bcryptjs.genSaltSync();
