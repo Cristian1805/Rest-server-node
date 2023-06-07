b69e8d875f275c3ff77282eabb767b1e303a80e6
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-06-07 10:59:02 -0500

    Validar todos los campos necesarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,12 +1,14 @@
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+
+const {validarCampos} = require('../middlewares/validar-campos')
 
 const { usuariosGet,
         usuariosPut,
         usuariosPost,
         usuariosDelete,
         usuariosPatch } = require('../controllers/usuarios');
-const { check } = require('express-validator');
 
 const router = Router();
 
@@ -16,7 +18,11 @@ router.get('/', usuariosGet );
 router.put('/:id', usuariosPut );
 
 router.post('/',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe contener minimo 6 caracteres').isLength({ min: 6}),
     check('correo', 'El correo no es valido').isEmail(),
+    check('rol', 'No es un rol valido').isIn(['ADMIN_ROL', 'USER_ROL']),
+    validarCampos 
 ], usuariosPost );
 
 router.delete('/', usuariosDelete );
