106255187c09e63374e2600dd24c55a02712328f
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-06-07 12:55:36 -0500

    Custom validation - EmailExiste

---
+++ helpers/db-validators.js
@@ -0,0 +1,22 @@
+const Role = require('../models/role');
+const Usuario = require('../models/usuario');
+
+const esRolValido = async(rol = '') => {
+    const existeRol = await Role.findOne({rol});
+    if (!existeRol){
+        throw new Error (`El rol ${ rol } no esta registrado en la Base de datos`) 
+    }
+}
+
+const emailExiste = async (correo = '') => {
+    //Verificar si el correo existe
+    const existeEmail = await Usuario.findOne({correo});
+    if(existeEmail){
+        throw new Error (`El correo: ${ correo}, ya esta registrado`);1
+    }
+}
+
+
+module.exports = {
+    esRolValido, emailExiste
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,9 +1,10 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const Role = require('../models/role');
+
 
 const {validarCampos} = require('../middlewares/validar-campos')
+const { esRolValido, emailExiste } = require('../helpers/db-validators');
 
 const { usuariosGet,
         usuariosPut,
@@ -22,15 +23,11 @@ router.post('/',[
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe contener minimo 6 caracteres').isLength({ min: 6}),
     check('correo', 'El correo no es valido').isEmail(),
+    check('correo').custom(emailExiste),
     //check('rol', 'No es un rol valido').isIn(['ADMIN_ROL', 'USER_ROL']),
 
 
-    check('rol').custom(async(rol = '') => {
-        const existeRol = await Role.findOne({rol});
-        if (!existeRol){
-            throw new Error (`El rol ${ rol } no esta registrado en la Base de datos`) 
-        }
-    }),
+    check('rol').custom( esRolValido ),
     validarCampos 
 ], usuariosPost );
 
