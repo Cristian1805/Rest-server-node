e56c5d4191e0b52a55de2caf836513208963ff10
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-06-22 13:27:15 -0500

    Validar Token de Google - Backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,10 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const bcryptjs  = require('bcryptjs');
 const Usuario = require('../models/usuario');
+
+
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response ) => {
 
@@ -60,10 +63,23 @@ const googlesignIn = async (req, res = response) => {
 
     const { id_token } = req.body;
 
-    res.json({
-        msg: 'Todo bien',
-        id_token
-    })
+    try {
+
+        const googleUser = await googleVerify( id_token );
+        console.log(googleUser);
+
+        res.json({
+            msg: 'Todo bien - Google SingIn exitoso',
+            id_token
+        })
+        
+    } catch (error) {
+        json.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+        
+    }
 
 }
 
