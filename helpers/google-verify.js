9ac20aec797a615bc565f7509fa79064eed58ac6
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-11-26 23:17:41 -0500

    CRUD y rutas de Categorías

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -6,7 +6,7 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 async function googleVerify( token = '') {
 
   const ticket = await client.verifyIdToken({
-      idToken: token,
+      idtoken: token,
       audience: process.env.GOOGLE_CLIENT_ID, 
        
   });
